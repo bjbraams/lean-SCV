caef1ae776ff79933718312357980d46628d3702
Bastiaan Braams <bjbraams@gmail.com>
2026-09-19 14:29:18 +0200

    Major upgrade.

--- github-math.lua
+++ github-math.lua
@@ -1,10 +1,14 @@
 function Math(el)
+  -- Convert GitHub-friendly \lt to < for Pandoc/LaTeX.
+  el.text = el.text:gsub("\\lt", "<")
+  -- Remove GitHub inline-math backticks.
   if el.mathtype == "InlineMath" then
     local s = el.text
     if #s >= 2 and s:sub(1,1) == "`" and s:sub(-1) == "`" then
       el.text = s:sub(2,-2)
     end
   end
+
   return el
 end
 
